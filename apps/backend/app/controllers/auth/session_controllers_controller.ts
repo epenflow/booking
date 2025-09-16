1e723552cb7a6c85b22cdc9c1f803df78b9e0996
epenflow <epenflow@gmail.com>
2025-09-16 11:55:36 +0800

    add dto package

---
+++ apps/backend/app/controllers/auth/session_controllers_controller.ts
@@ -0,0 +1,7 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class SessionControllersController {
+  async signUp(ctx: HttpContext) {}
+  async signIn(ctx: HttpContext) {}
+  async signOut(ctx: HttpContext) {}
+}
